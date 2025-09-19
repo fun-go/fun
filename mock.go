2bd53672ef3837ab76d77e5073dbbd6c61cff7dd
池易 <cygit config --global user.name Your>
2025-09-19 09:28:05 +0800

    删除多余

--- mock.go
+++ mock.go
@@ -43,7 +43,6 @@ func getMessage[T any](t *testing.T, id string) Result[T] {
 		// 清理请求信息
 		testRequestMap.Delete(id)
 		ErrorLogger(callError("fun:request timeout"))
-		logWg.Wait()
 		t.Fail()
 		return Result[T]{}
 	}
@@ -57,7 +56,6 @@ func mockSendJson(t *testing.T, requestInfo any) {
 }
 
 func MockRequest[T any](t *testing.T, requestInfo any) Result[T] {
-	defer logWg.Wait()
 	newClientOrService()
 	requestId := reflect.ValueOf(requestInfo).FieldByName("Id").String()
 	mockSendJson(t, requestInfo)
@@ -70,7 +68,6 @@ type On[T any] struct {
 }
 
 func MockProxyClose(t *testing.T, id string) {
-	defer logWg.Wait()
 	requestInfo := RequestInfo[any]{
 		Id:   id,
 		Type: CloseType,
@@ -79,7 +76,6 @@ func MockProxyClose(t *testing.T, id string) {
 }
 
 func MockProxy[T any](t *testing.T, requestInfo any, on On[T], seconds int64) {
-	defer logWg.Wait()
 	newClientOrService()
 	requestId := reflect.ValueOf(requestInfo).FieldByName("Id").String()
 
